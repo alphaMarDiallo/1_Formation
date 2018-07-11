5072475f48053708ebecafb4ebb77a2b4b0baeb2
alphaMarDiallo <alpha.diallo@lepoles.com>
2018-07-11 16:58:57 +0200

    20180711

---
+++ 02_jS/js/03_operateur_arithmétique.js
@@ -0,0 +1,20 @@
+// alert("js ready");
+/* ==== 1 - Les opérateurs === */
+/* ==== 1 - Les écritures simplifiées === */
+
+
+/* ==== 1 - Les opérateurs === */
+// Je déclaremes variables / costantes en début de fichier
+// je peux en déclarer plusieus à la voléd:
+var nb1, nb2, resultat;
+
+// J'affecte des valeurs à certaines variable
+nb1 = 10;
+nb2 = 5;
+
+/* -- 1 - Additionner avec '+' */
+resultat = nb1 + nb2;
+console.log(resultat);
+
+
+/* ==== 2 - Les écritures simplifiées === */
