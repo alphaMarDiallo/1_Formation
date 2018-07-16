c435f80ef2b7ed51b2797857c7ae49fe8966adbb
alphaMarDiallo <alpha.diallo@lepoles.com>
2018-07-16 17:01:37 +0200

    Js 20180716

--- 02_jS/999_exercices/jS/bonjour.js
+++
@@ -1,7 +0,0 @@
-// alert("bonjour js ready !");
-var votreNom = window.prompt("Quel est votre Nom ?");
-console.log(votreNom);
-
-var votrePrenom = window.prompt(" Votre prénom ?");
-console.log(votrePrenom);
-alert("Bonjour " + votrePrenom + " " + votreNom + " " + "!");
