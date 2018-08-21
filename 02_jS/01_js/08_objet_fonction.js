91a237f7bc24182611fd2d86cd024a062f79884e
alphaMarDiallo <alpha.diallo@lepoles.com>
2018-08-21 17:02:52 +0200

    20180821 JS WebF3

--- 02_jS/01_js/08_objet_fonction.js
+++ 02_jS/01_js/08_objet_fonction.js
@@ -55,3 +55,43 @@ console.log(dejeuner[6]);
      console.log("Aliment n° " + menu + ' ' + dejeuner[menu] );
      //console.log(dejeuner[menu]);
  }
+/* 
+FONCTIONS
+*/
+
+// Une fonction est une série d'instruction contenues dans des {accollades}
+//elle porte le nom que je lui donne
+// à chaque fois que j'en ai besoin je l'APPELLE
+// je déclare une fonction avec le mot-clé function
+
+//Une fonction anonyme ne prend aucun paramètre
+    //=> elle n'utilisera pas d'information personnalisée
+    function HelloTous(){
+        alert("Bonjour à tous !");
+    }
+    // pour exécuterma fonction il faut l'appeler :
+    HelloTous();
+
+    //Une fonction avec des paramètres ou arguments
+    function Pause(prenom){
+        document.write("<h2>" + prenom +" "+" part en pause ! </h2>");
+    }
+    //j'appelle ma fonction avec un paramètre 
+    Pause("Alpha");
+
+    function ecrit(e){
+        document.write("<h3>" +e+ "</h3>");
+    }
+    ecrit("N'importe quoi");
+
+    /* 
+    EXERCICE -Calculette
+    Ecrire une fonction qui additionne 2 chiffre passés en paramètre, le mot-clé RETURN de retourner le résultat des instruction codées
+    */
+   function calculatrice(nb1, nb2){
+    //var nb1 =prompt...=> var propre à ma fonction (elle n'existe que dans ma fonction)
+    //var nb2 = rompt...=> var propre à ma fonction (elle n'existe que dans ma fonction)
+    var resultat = nb1 + nb2;  
+    return resultat; 
+}
+calculatrice(8, 4);
