db502b8f9074cbf16f60167db3a700e8eaddcec3
alphaMarDiallo <alpha.diallo@lepoles.com>
2018-10-05 16:57:58 +0200

    php 20181005

--- 03_webforce3/02-back/php/08_site/inc/haut.inc.php
+++ 03_webforce3/02-back/php/08_site/inc/haut.inc.php
@@ -22,7 +22,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
         <!-- La marque -->
-       
+       <a href="<?php echo RACINE_SITE . 'boutique.php'; ?>" class="navbar-brand">MA BOUTIQUE</a>
        
         <!-- Le burger -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav1" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,8 +34,24 @@
         <div class="collapse navbar-collapse" id="nav1">
         	<ul class="navbar-nav ml-auto">
           <?php
-					
-				  ?>
+          echo '<li><a class="nav-link" href="' . RACINE_SITE . 'boutique.php">Boutique</a></li>';
+          //menu de l'internaute connecté :
+          if (internauteEstConnecte()) {
+            echo '<li><a class="nav-link" href="' . RACINE_SITE . 'profil.php">Profil</a></li>';
+            echo '<li><a class="nav-link" href="' . RACINE_SITE . 'connexion.php?action=deconnexion">Se deconnecter</a></li>';
+          } else {
+            // internaute non connecté :
+            echo '<li><a class="nav-link" href="' . RACINE_SITE . 'inscription.php">Inscription</a></li>';
+            echo '<li><a class="nav-link" href="' . RACINE_SITE . 'connexion.php">Connexion</a></li>';
+          }
+          echo '<li><a class="nav-link" href="' . RACINE_SITE . 'panier.php">Panier</a></li>';
+
+          //menu de l'administrateur : 
+          if (internauteEstConnecteEtAdmin()) {
+            echo '<li><a class="nav-link" href="' . RACINE_SITE . 'admin/gestion_boutique.php">Gestion de la boutique</a></li>';
+          }
+
+          ?>
         	</ul>
         </div>
       </div>
@@ -43,4 +59,15 @@
 
    
     <!-- Page Content -->
-    
+    <div class="container" style="min-height:80vh;">
+          <div class="row">
+            <div class="col-12">
+              <!-- ici le contenu spécifique de chaque page -->
+
+
+
+
+
+
+
+
