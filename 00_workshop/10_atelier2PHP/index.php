ed49d3c85e948523d101d6b9369abb35f9e4460f
alphaMarDiallo <alpha.diallo@lepoles.com>
2018-10-08 16:58:15 +0200

    php 20181008

--- 00_workshop/10_atelier2PHP/index.php
+++ 00_workshop/10_atelier2PHP/index.php
@@ -1,3 +1,11 @@
+<?php
+echo '<pre style="background-color: #d5ecd4 ;width: 1Ovw;">';
+echo '<strong>var_dump($param)</strong> <br>';
+var_dump($_GET);
+echo '</pre>';
+//require 'init.inc.php';
+$accueil = $_GET['action'];
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -27,15 +35,13 @@
         }
 
     </style>
-
-
 </head>
 <body>
     <!-- .container-fluid-->
    <div class="container-fluid p-0">
         <nav class="navbar navbar-expand-lg mb-4 fixed-top navbar-light">
             <div class="container">
-                <a class="navbar-brand" href="#"><i class="fab fa-phoenix-framework"></i> Phoenix</a>
+                <a class="navbar-brand" href="?action=home"><i class="fab fa-phoenix-framework"></i> Phoenix</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -45,10 +51,10 @@
                         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                     </li> -->
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Choisir une destination</a>
+                        <a class="nav-link" href="?action=choix">Choisir une destination</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Payer</a>
+                        <a class="nav-link" href="?action=payer">Payer</a>
                     </li>
                     </ul>
             </div>
@@ -57,6 +63,8 @@
                 </span> -->
             </div>
         </nav>
+    <?php
+if ($accueil === 'home') {?>
         <!-- carousel -->
         <div class="row">
             <div class="col">
@@ -109,7 +117,9 @@
          <!-- FIN carousel -->
         </div>
         <!-- bouton choisir mon séjour -->
-        <a href="#" class="btn btn-outline-info btn-md" role="button" aria-pressed="true">Choisir mon séjour tout de suite !</a>
+        <a href="?action=choix" class="btn btn-outline-info btn-md" role="button" aria-pressed="true">Choisir mon séjour tout de suite !</a>
+    <?php
+} else if ($_GET['action'] === 'choix') {?>
         <!-- container -->
         <div class="container">
            <div class="section-card">
@@ -122,7 +132,7 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Les Boucaniers</h5>
                                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-outline-info  btn-block">Réservez maintenant !</a>
+                                    <a href="?action=reserver" class="btn btn-outline-info  btn-block">Réservez maintenant !</a>
                                 </div>
                             </div>
                         </div>
@@ -132,7 +142,7 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Kamarina</h5>
                                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-outline-info  btn-block">Réservez maintenant !</a>
+                                    <a href="?action=reserver" class="btn btn-outline-info  btn-block">Réservez maintenant !</a>
                                 </div>
                             </div>
                         </div>
@@ -142,7 +152,7 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Finohlu</h5>
                                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-outline-info  btn-block">Réservez maintenant !</a>
+                                    <a href="?action=reserver" class="btn btn-outline-info  btn-block">Réservez maintenant !</a>
                                 </div>
                             </div>
                         </div>
@@ -156,7 +166,7 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Albion sauvage</h5>
                                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-outline-info  btn-block">Réservez maintenant !</a>
+                                    <a href="?action=reserver" class="btn btn-outline-info  btn-block">Réservez maintenant !</a>
                                 </div>
                             </div>
                         </div>
@@ -166,7 +176,7 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Kani</h5>
                                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-outline-info btn-block">Réservez maintenant !</a>
+                                    <a href="?action=reserver" class="btn btn-outline-info btn-block">Réservez maintenant !</a>
                                 </div>
                             </div>
                         </div>
@@ -176,7 +186,7 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Gregolimano</h5>
                                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-outline-info  btn-block">Réservez maintenant !</a>
+                                    <a href="?action=reserver" class="btn btn-outline-info  btn-block">Réservez maintenant !</a>
                                 </div>
                             </div>
                         </div>
@@ -187,6 +197,8 @@
            <div>
         <!-- FIN .container -->
         </div>
+        <?php
+} else if ($_GET['action'] === 'reserver') {?>
         <div class="row"> <!-- .row resa & image -->
             <!-- section-resa -->
                 <div class="col-md-4">
@@ -219,11 +231,13 @@
                                 </div>
                             </div>
                         </form>
-                            <button type="submit" class="btn btn-info btn-block">Confirmer ma réservation</button>
+                            <a href="?action=confirmer" class="btn btn-info btn-block">Confirmer ma réservation</a>
                         </div>
                     </div>
                 </div>
             <!--FIN section-resa -->
+             <?php
+} else if ($_GET['action'] === 'confirmer' || $_GET['action'] === 'payer') {?>
             <div class="row"> <!--section image -->
                 <div class="col-md-12">
                     <ul class="nav">
@@ -251,54 +265,86 @@
         </div><!--FIN .row resa & image-->
         <div class="row"> <!-- .row confirmation-->
             <div class="col-md-12"><!-- .col-md-12-->
-                <div class="card text-center"><!-- .card text-center-->
-                    <div class="card-header">
-                        <ul>
-                            <li><i class="fab fa-phoenix-framework"></i><p>Récapitulatif de votre réservation pour </p></li>
-                        </ul>
-                    </div>
-                    <div class="card-body">
-                        <div class="col-md-3">
-                            <div class="alert alert-warning" role="alert">
-                                 <p>participant(s) :</p>
-                            </div>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="alert alert-warning" role="alert">
-                                 <p>participant(s) :</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-footer text-muted">
-                       <ul class="nav justify-content-end">
-                        <li><p>Bon séjour </p><i class="fab fa-phoenix-framework"></i></li>
-                       </ul>
-                    </div>
-                </div><!-- .card text-center-->
+                <div class="alert alert-info" role="alert">
+                    A simple info alert—check it out!
+                </div>
             </div><!-- FIN .col-md-12-->
         </div><!-- FIN .row confirmation-->
+        <div class="row"><!--.row boutton récap-->
+            <div class="col-md-2">
+                <div class="alert alert-warning" role="alert">
+                    <p>Participant(s)</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="alert alert-warning" role="alert">
+
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="alert alert-success" role="alert">
+                    <p>Commande</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="alert alert-success" role="alert">
+
+                </div>
+            </div>
+        </div><!-- FIN .row boutton récap-->
+        <div class="row"><!--.row boutton récap-->
+            <div class="col-md-2">
+                <div class="alert alert-warning" role="alert">
+                    <p>Semaine(s)</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="alert alert-warning" role="alert">
+
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="alert alert-success" role="alert">
+                    <p>Total</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="alert alert-success" role="alert">
+
+                </div>
+            </div>
+        </div><!-- FIN .row boutton récap-->
+        <div class="row"> <!-- .row confirmation-->
+            <div class="col-md-12"><!-- .col-md-12-->
+                <div class="alert alert-info" role="alert">
+                    A simple info alert—check it out!
+                </div>
+            </div><!-- FIN .col-md-12-->
+        </div><!-- FIN .row confirmation-->
+            <?php
+}?>
         <footer>
             <div class="row mt-4">
                 <div class="col">
                     <div class="container">
                         <ul class="nav">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#"><i class="fas fa-umbrella-beach"></i>Vos vacances de rêves...</a>
+                                    <i class="fas fa-umbrella-beach">Vos vacances de rêves...</i>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#"><i class="fas fa-sun"></i>Plage...</a>
+                                    <i class="fas fa-sun">Plage...</i>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#"><i class="fas fa-building"></i>Urbaine...</a>
+                                    <i class="fas fa-building">Urbaine...</i>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Croisière...</a>
+                                    <i class="fas fa-ship">Croisière...</i>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#"><i class="fas fa-image"></i>Montagne...</a>
+                                    <i class="fas fa-image">Montagne...</i>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#"><i class="fas fa-euro-sign"></i>A pris tout doux...<i class="fas fa-umbrella-beach"></i></a>
+                                    <i class="fas fa-euro-sign">A pris tout doux...</i><i class="fas fa-umbrella-beach"></i>
                                 </li>
                                 <!-- <li class="nav-item">
                                     <a class="nav-link disabled" href="#">Disabled</a>
